0e2a07cbb354e39fd4da61a214d37ccebd5b625a
Ruslan <c8man24@gmail.com>
2022-11-23 13:43:18 +0300

    refactor: refactor the whole code

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,30 +8,14 @@ let initialState = {
   users: [],
   accounts: [
     {
-      id: 1,
       login: "admin",
       password: "admin",
       email: "admin@gmail.com",
     },
   ],
-  accountCounter: 1,
-  userCounter: 2,
+
   allowed: false,
 };
-
-// let newUser = {
-//   id: 4,
-//   firstName: "Ruslan",
-//   lastName: "Kharkevich",
-//   email: "mail@yandex.by",
-// };
-
-// let newAcc = {
-//   id: 2,
-//   login: "user",
-//   password: "user",
-//   email: "user@gmail.com",
-// };
 
 const store = createStore(
   combineReducers({ users, allowed, userCounter, accountCounter, accounts }),
